19fbf2df6c2bea0be8d7ac3bb4f95fb73d5e751c
joregesosa <joregesosa@gmail.com>
2026-04-05 00:54:36 -0400

    chore: project setup with Vite and Tailwind

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  publicDir: "../public",
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, "src/index.html"),
+        meals: resolve(__dirname, "src/meals/index.html"),
+        mealDetails: resolve(__dirname, "src/meals/meal-details.html"),
+      },
+    },
+  },
+});
